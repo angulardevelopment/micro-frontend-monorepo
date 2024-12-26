e78edf90f50c60ea0a79b6d67145f4dac58af145
Gupta, Sunny <sunnyvales789@gmail.com>
2024-12-26 14:22:46 +0530

    update

--- apps/demo/src/app/app.component.spec.ts
+++ apps/demo/src/app/app.component.spec.ts
@@ -3,11 +3,13 @@ import { AppComponent } from './app.component';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
+  let ctrl: HttpTestingController;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       declarations: [AppComponent],
     }).compileComponents();
+    ctrl = TestBed.inject(HttpTestingController);
   });
 
   it('should create the app', () => {
@@ -28,4 +30,20 @@ describe('AppComponent', () => {
   //   const compiled = fixture.nativeElement as HTMLElement;
   //   expect(compiled.querySelector('h1')?.textContent).toContain('Welcome demo');
   // });
+
+  it('can test HttpClient.get', () => {
+    const differValues: Data = { name: 'Differ Values' };
+  
+    httpClient.get<Data>(testUrl).subscribe(data =>
+      expect(data).toEqual(differValues)
+    );
+  
+    const unique = ctrl.expectOne('/data');
+    expect(unique.request.method).toEqual('GET');
+  
+    unique.flush(differValues);
+  
+    // Verify that no unmatched requests are outstanding
+    ctrl.verify();
+  });
 });
